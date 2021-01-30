109ea2c9f2f598f1b2672a1526ff429f60508456
cheef78 <45896881+cheef78@users.noreply.github.com>
2021-01-30 17:46:28 +0300

    Add files via upload

---
+++ HW1.js
@@ -0,0 +1,7 @@
+"use strict";
+let celsDegree = prompt('Введите значение температуры в градусах Цельсия');
+let farenDegree;
+//celsDegree = 15;
+farenDegree = (9 / 5) * celsDegree + 32;
+console.log(farenDegree);
+alert(`Значение температуры в градусах Цельсия = ${celsDegree} C` + `\nЗначение температуры в градусах Фаренгейта = ${farenDegree} F`);
