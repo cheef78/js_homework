fb570c125ab432a5c60df0f2eea9684544172a30
cheef78 <suslovoleg@mail.ru>
2021-01-31 16:22:54 +0300

    ghb

--- HW1.js
+++ HW1.js
@@ -1,7 +1,41 @@
 "use strict";
-let celsDegree = prompt('Введите значение температуры в градусах Цельсия');
-let farenDegree;
-//celsDegree = 15;
-farenDegree = (9 / 5) * celsDegree + 32;
-console.log(farenDegree);
-alert(`Значение температуры в градусах Цельсия = ${celsDegree} C` + `\nЗначение температуры в градусах Фаренгейта = ${farenDegree} F`);
+/*
+С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+*/
+let number = prompt('В диапазоне от 2 до N будут выведены все простые числа. Введите значение границы диапазона больше 0. N = ');
+while(true){
+    if (isNaN(number) == true) {
+    alert(`Вы ввели нечисловое значение. Это недопустимо !!!!`);
+    }
+    break; 
+}
+    
+
+function simpleNumber (number)
+{
+    let simpNumbs = []
+    if (number <= 1) {
+        alert(`Вы ввели значение границы меньше или = 1. Это недопустимо !!!!`);
+        }
+    
+    else {
+        let i =2;
+        while (i<= number) {
+            let n = parseInt(number**0.5)+1
+            
+            
+        }
+        
+        alert(`В выбранном вами диапазоне от 0 до ${number}, простыми являются числа ${simpNumbs}`);
+        }
+}
+
+simpleNumber (number) 
+
+
+    
+
+
+
+
+
