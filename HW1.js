4029565ebbff6f08cfa08494f86a3ee7fdd96550
cheef78 <suslovoleg@mail.ru>
2021-01-31 20:14:21 +0300

    история

--- HW1.js
+++ HW1.js
@@ -2,40 +2,78 @@
 /*
 С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 */
-let number = prompt('В диапазоне от 2 до N будут выведены все простые числа. Введите значение границы диапазона больше 0. N = ');
-while(true){
-    if (isNaN(number) == true) {
-    alert(`Вы ввели нечисловое значение. Это недопустимо !!!!`);
-    }
-    break; 
-}
-    
+let number = prompt('В диапазоне от 2 до N будут выведены все простые числа. Введите значение границы диапазона больше 1. N = ');
+
+
 
 function simpleNumber (number)
 {
     let simpNumbs = []
-    if (number <= 1) {
-        alert(`Вы ввели значение границы меньше или = 1. Это недопустимо !!!!`);
-        }
+    simpNumbs[0] = 2;
     
-    else {
-        let i =2;
-        while (i<= number) {
-            let n = parseInt(number**0.5)+1
-            
-            
+    while(true){
+        if (isNaN(number) == true || number <= 1)  {
+        alert(`Вы ввели нечисловое значение или число меньше 2. Это недопустимо !!!!`);
+        break;
         }
+        else if (number == 2) {
+            alert(`В выбранном вами диапазоне от 2 до ${number}, простыми являются числа ${simpNumbs}`);
+            break;
+            }
         
-        alert(`В выбранном вами диапазоне от 0 до ${number}, простыми являются числа ${simpNumbs}`);
+        else {
+            
+            
+            let divides = parseInt(number**0.5);
+            //
+            for (let i=3; i <= number;i++) {
+                let zeroCount = 0;
+                for(let j = 2; j <= divides; j++) {
+                    
+                    if (i % j == 0 & i != j  ) {
+                        zeroCount++;
+                        /*alert(`Текущее число =  ${i} Максимальный делитель =  ${divides} Текущий делитель =  ${j} Текущий остаток =  ${i % j} Текущее значение счетчиков 0 остатков от деления =  ${zeroCount}`);
+                        */
+                    }
+                    else {
+                        /*alert(`Текущее число =  ${i} Максимальный делитель =  ${divides} Текущий делитель =  ${j} Текущий остаток =  ${i % j} Текущее значение счетчиков 0 остатков от деления =  ${zeroCount}`);
+                        */
+                    }
+                    
+                }
+                
+                if (zeroCount == 0) {
+                    simpNumbs.push(i);
+                    }
+        
+                    
+            
+                    
+                  
+            }
+               
+            
+            alert(`В выбранном вами диапазоне от 2 до ${number}, простыми являются числа ${simpNumbs}`);
+
+            break;
+            
+         
         }
-}
+
+        
+
+        
+
+
+    }
+    
+}       
+        
+        
+        
+        
+        
+    
+
 
 simpleNumber (number) 
-
-
-    
-
-
-
-
-
