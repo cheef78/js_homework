de42f9ca2950df70a70fbc60ec6365188df51e0a
cheef78 <suslovoleg@mail.ru>
2021-01-31 20:14:21 +0300

    история

--- HW1.js
+++ HW1.js
@@ -3,31 +3,43 @@
 С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 */
 let number = prompt('В диапазоне от 2 до N будут выведены все простые числа. Введите значение границы диапазона больше 0. N = ');
-while(true){
-    if (isNaN(number) == true) {
-    alert(`Вы ввели нечисловое значение. Это недопустимо !!!!`);
-    }
-    break; 
-}
+
     
 
 function simpleNumber (number)
 {
-    let simpNumbs = []
-    if (number <= 1) {
-        alert(`Вы ввели значение границы меньше или = 1. Это недопустимо !!!!`);
+    while(true){
+        if (isNaN(number) == true || number <= 1)  {
+        alert(`Вы ввели нечисловое значение или число меньше 2. Это недопустимо !!!!`);
         }
-    
+        break;
+    }
+    let simpNumbs = []
+    let k = 0;
+    if (number == 2) {
+        simpNumbs[k] = 2;
+        alert(`В выбранном вами диапазоне от 2 до ${number}, простыми являются числа ${simpNumbs}`);
+            }
+        
     else {
         let i =2;
         while (i<= number) {
-            let n = parseInt(number**0.5)+1
+            let n = parseInt(number**0.5)+1;
+            let j=2;
+            while (j<= n) {
+                if 
+
+
+
+
+            }
             
             
         }
         
-        alert(`В выбранном вами диапазоне от 0 до ${number}, простыми являются числа ${simpNumbs}`);
+        
         }
+    
 }
 
 simpleNumber (number) 
